8dec752f3e91f8c722e15bf2f8be20ecdf9d3d62
Alina <alina.stolbikova.com>
2024-06-12 13:49:09 +0200

    refactoring.

--- src/components/Bookmarks.tsx
+++ src/components/Bookmarks.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { View } from "react-native";
-import { StackNavigationProp } from "@react-navigation/stack";
 
 import { useAppContext } from "../contexts/AppContext";
-import { RootStackParamList } from "../../App";
 import ImagesList from "./ImagesList";
 
 import styles from "../styles/bookmarksStyles";
-
-type ArtworksListNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  "ArtworkDetails"
->;
 
 const Bookmarks = () => {
   const { state } = useAppContext();
